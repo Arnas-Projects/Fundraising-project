a6100c2b767bda4fe4b772cd826805bab102c15a
Arnas-Projects <coding.myprojects@gmail.com>
2026-03-18 18:13:29 +0200

    added link in home dashboard for creating new story.

--- resources/views/stories/index.blade.php
+++ resources/views/stories/index.blade.php
@@ -1,5 +1,9 @@
 <h1>Lėšų rinkimo kampanijos</h1>
 
+<div style="width:400px; background:#eee; padding:20px; margin-bottom:40px; border-radius:5px; border:1px solid #ccc;">
+    <a href="{{ route('stories.create') }}">Sukurti naują kampaniją</a>
+</div>
+
 @foreach ($stories as $story)
     <div style="margin-bottom:40px; border-bottom:1px solid #ccc; padding-bottom:20px;">
 

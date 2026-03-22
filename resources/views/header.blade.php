f6e47a9c6be92ee550459c102573d463d20c736e
Arnas-Projects <coding.myprojects@gmail.com>
2026-03-22 19:49:03 +0200

    Commit

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,7 @@
 <div class="nav-wrapper">
     {{-- Project logo and name container --}}
     <div class="project-info-wrapper">
-        <img src="/images/logo.png" alt="Project Logo" class="project-logo" width="150">
-        <a href="/" class="project-logo">Fundraising Project</a>
+        <a href="/" class="project-logo">
+            <img src="{{ asset('images/beaver-funding.png') }}" alt="Project Logo" class="project-logo" width="100">
+        </a>
     </div>
